3adc2b30c0d96f45ffee1af3137cbadb865c6323
weasel130 <Indigo.Weasel.674@outlook.com>
2026-02-03 15:01:55 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ terraform {
   backend "s3" {
     bucket         = "jenkins-state-habib"   
     key            = "terraform.tfstate"
-    region         = "us-east-1"
+    region         = "us-west-2"
     encrypt        = true
   }
 }
